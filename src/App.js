ca6f3ef4b02701ad2611d6389a4dc9477c5ea16e
Isaac Scheff <isaac@Isaacs-MacBook-Pro.local>
2022-01-13 14:23:10 -0500

    Now pulls up random images instead of predetermined ones

--- src/App.js
+++ src/App.js
@@ -1,33 +1,24 @@
-import { useDispatch, useSelector } from "react-redux";
-import React, { useEffect, useState } from "react";
-import { fetchPics } from "./redux/pics";
+import React from "react";
 import SinglePic from "./nasaPic";
-import logo from './logo.svg';
 import './App.css';
 
+const getRandomDay = () => Math.floor(Math.random() * 27) + 1;
+
+const getRandomMonth = () => Math.floor(Math.random() * 11) + 1;
+
+const getRandomYear = () => Math.floor(Math.random() * 24) + 1996;
+
 function App() {
-
-  //   const dispatch = useDispatch();
-  //   const pic = useSelector((state) => state.pics);
-
-  //   useEffect(()=>{
-  //     dispatch(fetchPics())
-  // }, [])
-
-  // console.log(pic);
-
   return (
     <div className="App">
       <header> Earth Pics</header>
-      <SinglePic date='1996-06-21'/>
-      <SinglePic date='1995-08-22'/>
-      <SinglePic date='2004-01-11'/>
-      <SinglePic date='2014-12-25'/>
+      <SinglePic date={`${getRandomYear()}-${getRandomMonth()}-${getRandomDay()}`}/>
+      <SinglePic date={`${getRandomYear()}-${getRandomMonth()}-${getRandomDay()}`}/>
+      <SinglePic date={`${getRandomYear()}-${getRandomMonth()}-${getRandomDay()}`}/>
+      <SinglePic date={`${getRandomYear()}-${getRandomMonth()}-${getRandomDay()}`}/>
     </div>
   );
 }
 
 export default App;
 
-// API key 
-//  XkLxnoj1vglMVZed66K6qLvvR2scRzxrIcGgt4Fl
